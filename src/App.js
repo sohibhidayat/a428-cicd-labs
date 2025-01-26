d64277c178913b0eee88d34d87b69280ed06a8c1
Sohib Hidayat <sohibhidayat@gmail.com>
2025-01-27 02:00:31 +0800

    Modify App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import logo_idcamp from './idcamp.png';
+import logo_dicoding from './dicoding.png';
 
 class App extends Component {
   render() {
@@ -13,6 +15,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="App-add">
+        <p className="App-paragraph">
+          Ini adalah halaman project react-app sederhana<br/>
+          <img className="App-idcamp" src={logo_idcamp}/><br/>
+          <img className="App-idcamp" src={logo_dicoding}/>
+        </p>
+        </div>
       </div>
     );
   }
